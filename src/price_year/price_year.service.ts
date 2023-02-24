8eb2550a8a2d292b6fd19fcd02aecef3e2fa8804
abdulazizv <abdulazizheir@gmail.com>
2023-02-24 08:52:49 +0500

    service have been changing

--- src/price_year/price_year.service.ts
+++ src/price_year/price_year.service.ts
@@ -8,84 +8,87 @@ import { priceYear } from './schemas/price_year.model';
 
 @Injectable()
 export class PriceYearService {
-  constructor(@InjectModel(priceYear) private priceYearRepository:typeof priceYear) { }
+  constructor(
+    @InjectModel(priceYear) private priceYearRepository: typeof priceYear,
+  ) {}
 
   async create(createPriceYearDto: CreatePriceYearDto) {
-    const newpriceYear = await this.priceYearRepository.create(createPriceYearDto)
-    if(!newpriceYear){
+    const newpriceYear = await this.priceYearRepository.create(
+      createPriceYearDto,
+    );
+    if (!newpriceYear) {
       throw new HttpException(
-        "Error has been detected during save information",
-        HttpStatus.NOT_FOUND
-      )
+        'Error has been detected during save information',
+        HttpStatus.NOT_FOUND,
+      );
     }
-    return newpriceYear
+    return newpriceYear;
   }
 
   async findAll() {
-    const allPriceYear = await this.priceYearRepository.findAll({include:{all:true}})
-    if(!allPriceYear) {
+    const allPriceYear = await this.priceYearRepository.findAll({
+      include: { all: true },
+    });
+    if (!allPriceYear) {
       throw new HttpException(
-        "Information not found!Maybe Database is empty",
-        HttpStatus.NOT_FOUND
-      )
+        'Information not found!Maybe Database is empty',
+        HttpStatus.NOT_FOUND,
+      );
     }
-    return allPriceYear
+    return allPriceYear;
   }
 
   async findOne(id: number) {
     const onePriceYear = await this.priceYearRepository.findOne({
-      where:{
-        id:+id
-      }
-    })
-    if(!onePriceYear) {
-      throw new HttpException(
-        "Information not found",
-        HttpStatus.NOT_FOUND
-      )
+      where: {
+        id: +id,
+      },
+    });
+    if (!onePriceYear) {
+      throw new HttpException('Information not found', HttpStatus.NOT_FOUND);
     }
-    return onePriceYear
+    return onePriceYear;
   }
 
   async update(id: number, updatePriceYearDto: UpdatePriceYearDto) {
     const check = await this.priceYearRepository.findOne({
-      where:{
-        id:+id
-      }
-    })
-    if(!check){
+      where: {
+        id: +id,
+      },
+    });
+    if (!check) {
       throw new HttpException(
-        "ID is not correct! Information not found",
-        HttpStatus.NOT_FOUND
-      )
+        'ID is not correct! Information not found',
+        HttpStatus.NOT_FOUND,
+      );
     }
-    const newPriceYear = await this.priceYearRepository.update({
-      ...updatePriceYearDto
-    },{
-      where:{
-        id:+id
-      }
-    })
-    return newPriceYear
+    const newPriceYear = await this.priceYearRepository.update(
+      {
+        ...updatePriceYearDto,
+      },
+      {
+        where: {
+          id: +id,
+        },
+      },
+    );
+    return newPriceYear;
   }
 
   async remove(id: number) {
     const check = await this.priceYearRepository.findOne({
-      where:{
-        id:+id
-      }
-    })
-    if(!check){
-      throw new HttpException(
-        "Id is incorrect",
-        HttpStatus.NOT_FOUND
-      )
+      where: {
+        id: +id,
+      },
+    });
+    if (!check) {
+      throw new HttpException('Id is incorrect', HttpStatus.NOT_FOUND);
     }
     await this.priceYearRepository.destroy({
-      where:{
-        id:+id
-      }
-    })
-    return check.id
+      where: {
+        id: +id,
+      },
+    });
+    return check.id;
   }
 }
