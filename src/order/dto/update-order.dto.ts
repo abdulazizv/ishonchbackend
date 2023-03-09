241f04a03650436498c8e773b1a4d0d1c4a5ae4c
abdulazizv <abdulazizheir@gmail.com>
2023-03-09 09:47:33 +0500

    added new rows to order

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -3,22 +3,26 @@ import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreateOrderDto } from './create-order.dto';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
-    @IsOptional()
-    @IsNumber()
-    readonly product_id:number;
-    @IsOptional()
-    @IsNumber()
-    readonly discount_id:number;
-    @IsOptional()
-    @IsNumber()
-    readonly user_id:number;
-    @IsOptional()
-    @IsString()
-    readonly client_name:string;
-    @IsOptional()
-    @IsString()
-    readonly client_address:string;
-    @IsOptional()
-    @IsString()
-    readonly client_phone:string;
+  @IsOptional()
+  @IsNumber()
+  readonly product_id: number;
+  @IsOptional()
+  @IsNumber()
+  readonly discount_id: number;
+  @IsOptional()
+  @IsNumber()
+  readonly user_id: number;
+  @IsOptional()
+  @IsString()
+  readonly client_name: string;
+  @IsOptional()
+  @IsString()
+  readonly client_address: string;
+  @IsOptional()
+  @IsString()
+  readonly client_phone: string;
+
+  @IsOptional()
+  @IsString()
+  readonly device_id: string;
 }
